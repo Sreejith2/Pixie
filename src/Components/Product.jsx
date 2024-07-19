ab57163e057c615adea545d8b191702f6fc1475a
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2024-07-19 21:32:01 +0530

    made some changes

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -9,11 +9,11 @@ function Product() {
         <div className=' grid sm:grid-cols-2 items-center justify-center h-[400px] sm:h-[500px] border border-black px-10 sm:px-32'>
             <h1 className=' text-[30px] sm:text-[50px]'>Welcome to Pixie <span className=' text-[50px] sm:text-[70px] animate-waveHand'>👋</span></h1>
             <Carousel showArrows={false} showStatus={false} autoPlay interval={2000} showThumbs={false} transitionTime={1000} infiniteLoop>
-              <div className='flex bg-cover items-center bg-white'>
-                  <img src={bg} alt=''/>
+              <div className='flex bg-cover items-center h-[200px] sm:h-[350px] bg-white'>
+                  <img className='h-[200px] sm:h-[350px]' src={bg} alt=''/>
               </div>
-              <div className='flex bg-cover items-center bg-white'>
-                  <img src={pic} alt=''/>
+              <div className='flex h-[200px] sm:h-[350px] bg-cover items-center bg-white'>
+                  <img className='h-[200px] sm:h-[350px]' src={pic} alt=''/>
               </div>
             </Carousel>
         </div>
