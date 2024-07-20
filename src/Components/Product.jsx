c2e9e0e645f89b8da85bb88e996e3c31a8a9ce5a
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2024-07-20 10:44:21 +0530

    updated product page

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import bg from "../assets/Pixie.png"
-import pic from "../assets/pixie-1.png"
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import trends from "../assets/images/trends.svg"
+import { Link } from 'react-router-dom'
 function Product() {
   return (
-    <div className='flex flex-1 bg-black text-white w-screen flex-col'>
-        <div className=' grid sm:grid-cols-2 items-center justify-center h-[400px] sm:h-[500px] border border-black px-10 sm:px-32'>
+    <div className='flex flex-1 bg-black text-white w-screen flex-col items-center'>
+        <div className=' w-[20%] sm:w-[10%] relative group mt-10'>
+          <div className=' w-[100%] -inset-1 group-hover:opacity-100 transition duration-200 opacity-75 absolute bg-gradient-to-r from-pink-600 to-purple-600 blur rounded-lg'></div>
+          <Link to={"/login"}>
+            <button className=' px-2 sm:px-7 relative inset-0 py-3 text-[14px] sm:text-[16px] sm:py-4 w-[95%] bg-black rounded-lg leading-none justify-center flex items-center text-gray-100 '>Get started</button>
+          </Link>
+        </div>
+        <div className='grid sm:grid-cols-2 items-center justify-center h-[400px] sm:h-[400px] border border-black px-10 sm:px-32'>
             <h1 className=' text-[30px] sm:text-[50px]'>Welcome to Pixie <span className=' text-[50px] sm:text-[70px] animate-waveHand'>👋</span></h1>
-            <Carousel showArrows={false} showStatus={false} autoPlay interval={2000} showThumbs={false} transitionTime={1000} infiniteLoop>
-              <div className='flex bg-cover items-center h-[200px] sm:h-[350px] bg-white'>
-                  <img className='h-[200px] sm:h-[350px]' src={bg} alt=''/>
-              </div>
-              <div className='flex h-[200px] sm:h-[350px] bg-cover items-center bg-white'>
-                  <img className='h-[200px] sm:h-[350px]' src={pic} alt=''/>
-              </div>
-            </Carousel>
+            <div>
+              <img className=' h-52 sm:h-64' src={trends} alt=''/>
+            </div>
         </div>
     </div>
   )
